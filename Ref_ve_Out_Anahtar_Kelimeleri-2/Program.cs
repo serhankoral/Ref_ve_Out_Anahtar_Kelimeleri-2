6cb44a17cd1d51dfba60e6180989de7450296513
Serhan KORAL <serhan_koral@hotmail.com>
2023-01-19 16:21:01 +0300

    Proje dosyası ekle.

---
+++ Ref_ve_Out_Anahtar_Kelimeleri-2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ref_ve_Out_Anahtar_Kelimeleri_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bölme işleminde bölüm ve kalanı bulma
+            int bolunen = 13;
+            int x = bolunen;
+            int bolen = 4;
+            int kalan=0;
+            int bolum=0;
+            
+            for(int i = 0; i < int.MaxValue; i++)
+            {
+                if(bolunen>=bolen)
+                {
+                    bolunen = bolunen - bolen;
+                    bolum++;
+                }
+                else
+                {
+                    kalan = (x - bolum * bolen);
+                    break;
+                }
+            }
+            Console.WriteLine("Bolünen={0}", x);
+            Console.WriteLine("Bölen={0}", bolen);
+            Console.WriteLine("Bolüm={0}", bolum);
+            Console.WriteLine("Kalan={0}", kalan);
+        }
+        static void bol(int bolunen,int bolen, ref int bolum, ref int kalan)
+        {
+
+        }
+    }
+}
